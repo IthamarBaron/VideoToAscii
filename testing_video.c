e2f4fa772952bab90882719047329635c68579ff
Ithamar <57110382+IthamarBaron@users.noreply.github.com>
2025-02-25 21:07:35 +0200

    video to frames conversion
    using ffmpeg.exe to convert video to frames and save them in a folder

---
+++ testing_video.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define VIDEO_PATH "C:\\Users\\itham\\Desktop\\temp\\badApple.mp4"
+#define OUTPUT_FOLDER "frames"
+#define FPS 10
+#define SCALE_WIDTH 100
+#define SCALE_HEIGHT 50
+
+
+void DeleteFolder(const char* folder) {
+    char command[256];
+    snprintf(command, sizeof(command), "rmdir /s /q %s", folder);
+    system(command);
+}
+
+void ExtractFrames() {
+    char command[256];
+
+    //Reload the frames folder
+    DeleteFolder(OUTPUT_FOLDER);
+    _mkdir(OUTPUT_FOLDER);
+
+    // Construct the FFmpeg command to extract frames (and close cmd after execution)
+    snprintf(command, sizeof(command),
+        "cmd /c ffmpeg -i %s -vf \"fps=%d,scale=%d:%d\" %s/frame_%%04d.png",
+        VIDEO_PATH, FPS, SCALE_WIDTH, SCALE_HEIGHT, OUTPUT_FOLDER);
+
+
+    printf("Running command: %s\n", command);
+
+    // Run the command
+    system(command);
+}
+
+
+int main() {
+    printf("Extracting frames...\n");
+    ExtractFrames();
+    printf("Frames extracted!\n");
+    return 0;
+}
